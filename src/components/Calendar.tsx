fbbbcc82cbf33854cd8413090d6f322b1f9680d5
Ali-Dzhan Ali <adzh.aliali@gmail.com>
2023-04-19 15:54:28 +0300

    Fixed Calendar bugs

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,69 +3,70 @@ import dayjs, { Dayjs } from "dayjs";
 import { ChevronFirst, ChevronLast } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 
-function leapYear(year: number) {
-  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-}
-
-const currentYear = dayjs().year();
-const currentMonth = dayjs().month();
-const isLeapYear = leapYear(currentYear);
-
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sept",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-const numberOfDaysInMonths = [
-  31,
-  isLeapYear ? 29 : 28,
-  31,
-  30,
-  31,
-  30,
-  31,
-  31,
-  30,
-  31,
-  30,
-  31,
-];
-
-interface MonthDaysProps {
-  day: number;
-  dayOfWeek: string;
-}
-
-// 0-11 format
-function getMonth(month: number, year: number) {
-  const numberOfDays = numberOfDaysInMonths[month];
-  const monthCalendar: MonthDaysProps[] = [];
-  for (let i = 1; i < numberOfDays + 1; i++) {
-    const dayString = `${month + 1}-${i}-${year}`;
-    const day: Dayjs = dayjs(dayString, "M-D-YYYY");
-    monthCalendar.push({
-      day: i,
-      dayOfWeek: day.$d.toString().substr(0, 3),
-    });
-  }
-  return monthCalendar;
-}
-
 const Calendar = () => {
+  function leapYear(year: number) {
+    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+  }
+
+  const currentYear = dayjs().year();
+  const currentMonth = dayjs().month();
+  const isLeapYear = leapYear(currentYear);
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const numberOfDaysInMonths = [
+    31,
+    isLeapYear ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  interface MonthDaysProps {
+    day: number;
+    dayOfWeek: string;
+  }
+
+  // 0-11 format
+  function getMonth(month: number, year: number) {
+    const isLeapYear = leapYear(year);
+    const numberOfDays = numberOfDaysInMonths[month];
+    const monthCalendar: MonthDaysProps[] = [];
+    for (let i = 1; i < numberOfDays + 1; i++) {
+      const dayString = `${month + 1}-${i}-${year}`;
+      const day: Dayjs = dayjs(dayString, "M-D-YYYY");
+      monthCalendar.push({
+        day: i,
+        dayOfWeek: day.$d.toString().substr(0, 3),
+      });
+    }
+    return monthCalendar;
+  }
+  // set state for Month and Year on top of Calendar
   const [calendarMonth, setCalendarMonth] = useState(currentMonth);
   const [calendarYear, setCalendarYear] = useState(currentYear);
 
-  // get full calendar year
+  // function to obtain a full calendar year
   const getFullCalendarYear = (year: number) => {
     const tempCalendar: Array<MonthDaysProps[]> = [];
     for (let i = 0; i < 12; i++) {
@@ -75,30 +76,31 @@ const Calendar = () => {
     return tempCalendar;
   };
 
-  const fullCalendar = getFullCalendarYear(currentYear);
-
+  //
+  const fullCalendarYear = getFullCalendarYear(calendarYear);
   // get calendar for current month
-  const [calendar, setCalendar] = useState(fullCalendar[calendarMonth]);
+  const [calendar, setCalendar] = useState(fullCalendarYear[calendarMonth]);
 
   // functions to handle month change
   const handlePrevMonth = () => {
-    if (calendarMonth === 0) {
-      setCalendarMonth(11);
-      setCalendarYear(calendarYear - 1);
-      // handle calendar for previous year
-    } else {
+    if (calendarMonth !== 0) {
       setCalendarMonth(calendarMonth - 1);
-      setCalendar(fullCalendar[calendarMonth - 1]);
+      setCalendar(fullCalendarYear[calendarMonth - 1]);
+    } else {
+      setCalendar(getFullCalendarYear(calendarYear - 1)[11]);
+      setCalendarYear(calendarYear - 1);
+      setCalendarMonth(11);
     }
   };
   const handleNextMonth = () => {
-    if (calendarMonth === 11) {
-      setCalendarMonth(0);
-      setCalendarYear(calendarYear + 1);
+    if (calendarMonth !== 11) {
+      setCalendarMonth(calendarMonth + 1);
+      setCalendar(fullCalendarYear[calendarMonth + 1]);
       // handle calendar for next year
     } else {
-      setCalendarMonth(calendarMonth + 1);
-      setCalendar(fullCalendar[calendarMonth + 1]);
+      setCalendar(getFullCalendarYear(calendarYear + 1)[0]);
+      setCalendarYear(calendarYear + 1);
+      setCalendarMonth(0);
     }
   };
 
